fbf9f46dad1235f2ed9dc69c974a11d1c4192ccd
supriyaphalle <supriyaphalle@gmail.com>
2020-03-14 11:00:48 +0530

    ADDED:Welcome message

---
+++ src/main/java/quantitymeasurementmockito/QuantityMeasurement.java
@@ -0,0 +1,9 @@
+package quantitymeasurementmockito;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Measurement");
+
+    }
+}
+
---
+++ src/test/java/quantitymeasurementmockito/QuantityMeasurementTest.java
@@ -0,0 +1,18 @@
+package quantitymeasurementmockito;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement = null;
+
+    @Before
+    public void setup() {
+        quantityMeasurement = new QuantityMeasurement();
+    }
+
+    @Test
+    public void UC1() {
+
+    }
+}
