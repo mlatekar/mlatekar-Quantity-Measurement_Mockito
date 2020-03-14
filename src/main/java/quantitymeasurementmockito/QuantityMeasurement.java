c2af6f91fba891751b00c91ec5837df42aaca1de
supriyaphalle <supriyaphalle@gmail.com>
2020-03-14 17:37:04 +0530

    ADDED:Code for inch equality

--- src/main/java/quantitymeasurementmockito/QuantityMeasurement.java
+++ src/main/java/quantitymeasurementmockito/QuantityMeasurement.java
@@ -1,26 +1,28 @@
 package quantitymeasurementmockito;
 
-import com.sun.org.apache.xpath.internal.operations.Equals;
-
 import java.text.DecimalFormat;
+import java.util.Objects;
 
 public class QuantityMeasurement {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Quantity Measurement");
 
-    }
-    private static DecimalFormat decimalFormat=new DecimalFormat("#.##");
-
-    public double feetTOInchConversion(double feet) {
-        double inch=feet*12.0;
-        return Double.parseDouble(decimalFormat.format(inch));
-    }
+    private int second;
+    private int first;
+    QuantityMeasurement quantityMeasurement = null;
 
     public boolean feetCheck(int first, int second) {
-    return (first == second);
-
+        return (first == second);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Objects.equals(quantityMeasurement, that.quantityMeasurement);
     }
 
+    public boolean inchcheck(int first, int second) {
+        return (first == second);
+    }
 
 }
 
--- src/test/java/quantitymeasurementmockito/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurementmockito/QuantityMeasurementTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class QuantityMeasurementTest {
     QuantityMeasurement quantityMeasurement = null;
 
@@ -13,15 +14,77 @@ public class QuantityMeasurementTest {
     }
 
     @Test
-    public void givenFeet_ShouldReturn_Inch() {
-        double inch = quantityMeasurement.feetTOInchConversion(1.2);
-        Assert.assertEquals(14.4,inch,0.0d);
-    }
-
-
-    @Test
-    public void givenZeroFeet_ShouldReturn_Equal() {
+    public void givenZeroFeet_WhenEquals_ShouldReturn_True() {
         boolean check = quantityMeasurement.feetCheck(0, 0);
         Assert.assertTrue(check);
     }
+    @Test
+    public void givenFeet_WhenNotEquals_ShouldReturn_False() {
+        boolean check = quantityMeasurement.feetCheck(0, 1);
+        Assert.assertFalse(check);
+    }
+
+    @Test
+    public void givenNull_WhenNotEquals_ShouldReturn_False() {
+        Integer feet=null;
+        Assert.assertFalse(quantityMeasurement.equals(feet));
+    }
+
+    @Test
+    public void given_ReferenceCheck_ShouldReturn_True() {
+        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement();
+        Assert.assertEquals(quantityMeasurement1,quantityMeasurement);
+    }
+
+    @Test
+    public void given_TypeCheck_ShouldReturn_True() {
+        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement();
+        Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement1));
+
+    }
+
+    @Test
+    public void given_ValueCheckFor_Equality_ShouldReturn_True() {
+        boolean check = quantityMeasurement.feetCheck(2, 2);
+        Assert.assertTrue(check);
+    }
+
+    @Test
+    public void givenZero_Inch_WhenEquals_ShouldReturn_True() {
+        boolean check = quantityMeasurement.inchcheck(0, 0);
+        Assert.assertTrue(check);
+    }
+    @Test
+    public void givenFeet_Inch_WhenNotEquals_ShouldReturn_False() {
+        boolean check = quantityMeasurement.inchcheck(0, 1);
+        Assert.assertFalse(check);
+    }
+
+    @Test
+    public void givenNull_WhenNotEqualsInch_ShouldReturn_False() {
+        Integer inch=null;
+        Assert.assertFalse(quantityMeasurement.equals(inch));
+    }
+
+    @Test
+    public void given_ReferenceCheckInch_ShouldReturn_True() {
+        QuantityMeasurement inch = new QuantityMeasurement();
+        Assert.assertEquals(inch,quantityMeasurement);
+    }
+
+    @Test
+    public void given_TypeCheckInch_ShouldReturn_True() {
+        QuantityMeasurement inch1 = new QuantityMeasurement();
+        Assert.assertTrue(quantityMeasurement.equals(inch1));
+
+    }
+
+    @Test
+    public void given_ValueCheckForInch_Equality_ShouldReturn_True() {
+        boolean checkInch = quantityMeasurement.inchcheck(2, 2);
+        Assert.assertTrue(checkInch);
+
+    }
+
+
 }
